2a00cdc3ab952f433f0c6acca70a7e6a3d5ef9cf
ANUBHAV SRIVASTAVA <50038824+AnubhavSrivastavaGithub@users.noreply.github.com>
2020-07-20 13:51:50 +0530

    Create CurrentTemplate.cpp

---
+++ CurrentTemplate.cpp
@@ -0,0 +1,27 @@
+//This Code is written by Anubhav Srivastava(anubhavsrivastavawebpage.wordpress.com)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef double db;
+
+//Defines
+#define pb push_back
+#define f(n) for(ll i=0;i<n;i++)
+#define fl(i,a,b) for(ll i=a;i<b;i++)
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+//Important Functions
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define min(a, b) (((a)<(b))?(a):(b))
+#define max(a, b) (((a)<(b))?(b):(a))
+
+//Templates
+
+//Main Function
+int main() {
+	  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	return 0;
+}
